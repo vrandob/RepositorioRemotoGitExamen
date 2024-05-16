425878c77f2b4fe0c0264f867e0f0aff1efd3ae6
vrand <vrand@NitroVR>
2024-05-16 18:22:22 +0200

    cambio en rama1

---
+++ ProyectoGitExamen/src/paqueteGitExamen/ClaseRama1.java
@@ -0,0 +1,11 @@
+package paqueteGitExamen;
+
+public class ClaseRama1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mensaje 1 de la rama1");
+		System.out.println("mensaje 2 de la rama1");
+	}
+
+}
