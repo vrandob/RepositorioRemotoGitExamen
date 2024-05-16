e39ab86d4c4e0fc307f3ca5dc43192435db26d55
vrand <vrand@NitroVR>
2024-05-16 18:04:54 +0200

    Confirmar cambios

---
+++ ProyectoGitExamen/src/paqueteGitExamen/ClaseMensaje.java
@@ -0,0 +1,10 @@
+package paqueteGitExamen;
+
+public class ClaseMensaje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejercicio 3 del último examen");
+	}
+
+}
